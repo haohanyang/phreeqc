2e2d5eb0f021b7028e5c516ba0bd612053125a29
Haohan Yang <yanghaohanmail@gmail.com>
2024-11-06 23:41:40 +0100

    add namespace

--- src/main.cpp
+++ src/main.cpp
@@ -4,104 +4,107 @@
 
 namespace py = pybind11;
 
-struct Phreeqc
+namespace Bindings
 {
-    Phreeqc()
+    struct Phreeqc
     {
-        instance = new IPhreeqc();
-    }
-
-    ~Phreeqc()
-    {
-        if (instance != nullptr)
+        Phreeqc()
         {
-            delete instance;
+            instance = new IPhreeqc();
         }
-    }
 
-    void loadDatabase(const char *path)
-    {
-        int errors = instance->LoadDatabase(path);
-        if (errors > 0)
+        ~Phreeqc()
         {
-            throw std::runtime_error("Failed to load database");
-        }
-    }
-
-    void runString(const char *input)
-    {
-        int errors = instance->RunString(input);
-        if (errors > 0)
-        {
-            throw std::runtime_error("Failed to run string");
-        }
-    }
-
-    void runFile(const char *path)
-    {
-        int errors = instance->RunFile(path);
-        if (errors > 0)
-        {
-            throw std::runtime_error("Failed to run path");
-        }
-    }
-
-    const py::list getSelectedOutput()
-    {
-        py::list output;
-
-        VAR var;
-        VarInit(&var);
-
-        const int colCount = instance->GetSelectedOutputColumnCount();
-        const int rowCount = instance->GetSelectedOutputRowCount();
-
-        for (int row = 0; row < rowCount; row++)
-        {
-            py::list rowData;
-            for (int col = 0; col < colCount; col++)
+            if (instance != nullptr)
             {
-                if (instance->GetSelectedOutputValue(row, col, &var) != VR_OK)
+                delete instance;
+            }
+        }
+
+        void loadDatabase(const char *path)
+        {
+            int errors = instance->LoadDatabase(path);
+            if (errors > 0)
+            {
+                throw std::runtime_error("Failed to load database");
+            }
+        }
+
+        void runString(const char *input)
+        {
+            int errors = instance->RunString(input);
+            if (errors > 0)
+            {
+                throw std::runtime_error("Failed to run string");
+            }
+        }
+
+        void runFile(const char *path)
+        {
+            int errors = instance->RunFile(path);
+            if (errors > 0)
+            {
+                throw std::runtime_error("Failed to run path");
+            }
+        }
+
+        const py::list getSelectedOutput()
+        {
+            py::list output;
+
+            VAR var;
+            VarInit(&var);
+
+            const int colCount = instance->GetSelectedOutputColumnCount();
+            const int rowCount = instance->GetSelectedOutputRowCount();
+
+            for (int row = 0; row < rowCount; row++)
+            {
+                py::list rowData;
+                for (int col = 0; col < colCount; col++)
                 {
-                    rowData.append(py::str("error"));
-                }
-                else
-                {
-                    if (var.type == TT_LONG)
+                    if (instance->GetSelectedOutputValue(row, col, &var) != VR_OK)
                     {
-                        rowData.append(py::int_(var.lVal));
-                    }
-                    else if (var.type == TT_DOUBLE)
-                    {
-                        rowData.append(py::float_(var.dVal));
-                    }
-                    else if (var.type == TT_STRING)
-                    {
-                        rowData.append(py::str(var.sVal));
+                        rowData.append(py::str("error"));
                     }
                     else
                     {
-                        rowData.append(py::none());
+                        if (var.type == TT_LONG)
+                        {
+                            rowData.append(py::int_(var.lVal));
+                        }
+                        else if (var.type == TT_DOUBLE)
+                        {
+                            rowData.append(py::float_(var.dVal));
+                        }
+                        else if (var.type == TT_STRING)
+                        {
+                            rowData.append(py::str(var.sVal));
+                        }
+                        else
+                        {
+                            rowData.append(py::none());
+                        }
                     }
                 }
+
+                output.append(rowData);
             }
 
-            output.append(rowData);
+            return output;
         }
 
-        return output;
-    }
-
-    IPhreeqc *instance;
-};
+        IPhreeqc *instance;
+    };
+}
 
 PYBIND11_MODULE(phreeqc, m)
 {
     m.doc() = "Python bindings for PHREEQC Version 3";
-    py::class_<Phreeqc>(m, "Phreeqc")
+    py::class_<Bindings::Phreeqc>(m, "Phreeqc")
         .def(py::init<>())
-        .def("load_database", &Phreeqc::loadDatabase)
-        .def("run_string", &Phreeqc::runString)
-        .def("run_file", &Phreeqc::runFile)
-        .def("get_selected_output", &Phreeqc::getSelectedOutput);
+        .def("load_database", &Bindings::Phreeqc::loadDatabase)
+        .def("run_string", &Bindings::Phreeqc::runString)
+        .def("run_file", &Bindings::Phreeqc::runFile)
+        .def("get_selected_output", &Bindings::Phreeqc::getSelectedOutput);
 }
