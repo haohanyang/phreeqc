6cac402833bf59f6e0859888b7c1d40463611c5a
Haohan Yang <yanghaohanmail@gmail.com>
2024-11-22 18:32:06 +0100

    Add more iphreeqc methods

--- src/main.cpp
+++ src/main.cpp
@@ -1,116 +1,157 @@
+#include <variant>
 #include <exception>
+#include <string>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include "IPhreeqc.hpp"
 
 namespace py = pybind11;
 
-namespace Bindings
+class _Phreeqc : public IPhreeqc
 {
-    struct Phreeqc
+public:
+    void _AccumulateLine(const char *line)
     {
-        Phreeqc()
+        auto result = AccumulateLine(line);
+        if (result == VR_OUTOFMEMORY)
         {
-            instance = new IPhreeqc();
+            throw std::runtime_error("Failure, Out of memory");
         }
+    }
+    const std::variant<long, double, std::string> _GetSelectedOutputValue(int row, int col)
+    {
 
-        ~Phreeqc()
+        VAR var;
+        VarInit(&var);
+        auto result = GetSelectedOutputValue(row, col, &var);
+
+        if (result == VR_OK)
         {
-            if (instance != nullptr)
+            if (var.type == TT_LONG)
             {
-                delete instance;
+                return var.lVal;
+            }
+            else if (var.type == TT_DOUBLE)
+            {
+                return var.dVal;
+            }
+            else if (var.type == TT_STRING)
+            {
+                return std::string(var.sVal);
+            }
+            else if (var.type == TT_EMPTY)
+            {
+                throw std::runtime_error("EMPTY value");
+            }
+            else
+            {
+                throw std::runtime_error("ERROR value");
             }
         }
-
-        void loadDatabase(const char *path)
+        else if (result == VR_OUTOFMEMORY)
         {
-            int errors = instance->LoadDatabase(path);
-            if (errors > 0)
-            {
-                throw std::runtime_error("Failed to load database");
-            }
+            throw std::runtime_error("Failure, Out of memory");
         }
-
-        void runString(const char *input)
+        else if (result == VR_BADVARTYPE)
         {
-            int errors = instance->RunString(input);
-            if (errors > 0)
-            {
-                throw std::runtime_error("Failed to run string");
-            }
+            throw std::runtime_error("Failure, Invalid VAR type");
         }
-
-        void runFile(const char *path)
+        else if (result == VR_INVALIDARG)
         {
-            int errors = instance->RunFile(path);
-            if (errors > 0)
-            {
-                throw std::runtime_error("Failed to run path");
-            }
+            throw std::runtime_error("Failure, Invalid argument");
         }
-
-        const py::list getSelectedOutput()
+        else if (result == VR_INVALIDROW)
         {
-            py::list output;
-
-            VAR var;
-            VarInit(&var);
-
-            const int colCount = instance->GetSelectedOutputColumnCount();
-            const int rowCount = instance->GetSelectedOutputRowCount();
-
-            for (int row = 0; row < rowCount; row++)
-            {
-                py::list rowData;
-                for (int col = 0; col < colCount; col++)
-                {
-                    if (instance->GetSelectedOutputValue(row, col, &var) != VR_OK)
-                    {
-                        rowData.append(py::str("error"));
-                    }
-                    else
-                    {
-                        if (var.type == TT_LONG)
-                        {
-                            rowData.append(py::int_(var.lVal));
-                        }
-                        else if (var.type == TT_DOUBLE)
-                        {
-                            rowData.append(py::float_(var.dVal));
-                        }
-                        else if (var.type == TT_STRING)
-                        {
-                            rowData.append(py::str(var.sVal));
-                        }
-                        else
-                        {
-                            rowData.append(py::none());
-                        }
-                    }
-                }
-
-                output.append(rowData);
-            }
-
-            return output;
+            throw std::runtime_error("Invalid row");
         }
-
-        IPhreeqc *instance;
-    };
-}
+        else
+        {
+            throw std::runtime_error("Invalid column");
+        }
+    }
+};
 
 PYBIND11_MODULE(_iphreeqc, m)
 {
-    m.doc() = "Python bindings for PHREEQC Version 3";
-    py::class_<Bindings::Phreeqc>(m, "Phreeqc")
-        .def(py::init<>())
-        .def("load_database", &Bindings::Phreeqc::loadDatabase)
-        .def("run_string", &Bindings::Phreeqc::runString)
-        .def("run_file", &Bindings::Phreeqc::runFile)
-        .def("get_selected_output", &Bindings::Phreeqc::getSelectedOutput);
 
-#ifdef VERSION_INFO
-    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
-#else
-    m.attr("__version__") = "dev";
-#endif
+    m.doc() = "Python bindings for PHREEQC Version 3";
+    py::class_<_Phreeqc>(m, "_Phreeqc")
+        .def(py::init<>())
+        .def("accumulate_line", &_Phreeqc::_AccumulateLine)
+        .def("add_error", &_Phreeqc::AddError)
+        .def("add_warning", &_Phreeqc::AddWarning)
+        .def("clear_accumulated_lines", &_Phreeqc::ClearAccumulatedLines)
+        .def("get_accumulated_lines", &_Phreeqc::GetAccumulatedLines)
+        .def("get_component", &_Phreeqc::GetComponent)
+        .def("get_component_count", &_Phreeqc::GetComponentCount)
+        .def("get_current_selected_output_user_number", &_Phreeqc::GetCurrentSelectedOutputUserNumber)
+        .def("get_dump_file_name", &_Phreeqc::GetDumpFileName)
+        .def("get_dump_file_on", &_Phreeqc::GetDumpFileOn)
+        .def("get_dump_string", &_Phreeqc::GetDumpString)
+        .def("get_dump_string_line", &_Phreeqc::GetDumpStringLine)
+        .def("get_dump_string_line_count", &_Phreeqc::GetDumpStringLineCount)
+        .def("get_dump_string_on", &_Phreeqc::GetDumpStringOn)
+        .def("get_error_file_name", &_Phreeqc::GetErrorFileName)
+        .def("get_error_file_on", &_Phreeqc::GetErrorFileOn)
+        .def("get_error_on", &_Phreeqc::GetErrorOn)
+        .def("get_error_string", &_Phreeqc::GetErrorString)
+        .def("get_error_string_line", &_Phreeqc::GetErrorStringLine)
+        .def("get_error_string_line_count", &_Phreeqc::GetErrorStringLineCount)
+        .def("get_error_string_on", &_Phreeqc::GetErrorStringOn)
+        .def("get_id", &_Phreeqc::GetId)
+        .def("get_log_file_name", &_Phreeqc::GetLogFileName)
+        .def("get_log_file_on", &_Phreeqc::GetLogFileOn)
+        .def("get_log_string", &_Phreeqc::GetLogString)
+        .def("get_log_string_line", &_Phreeqc::GetLogStringLine)
+        .def("get_log_string_line_count", &_Phreeqc::GetLogStringLineCount)
+        .def("get_log_string_on", &_Phreeqc::GetLogStringOn)
+        .def("get_nth_selected_output_user_number", &_Phreeqc::GetNthSelectedOutputUserNumber)
+        .def("get_output_file_name", &_Phreeqc::GetOutputFileName)
+        .def("get_output_file_on", &_Phreeqc::GetOutputFileOn)
+        .def("get_output_string", &_Phreeqc::GetOutputString)
+        .def("get_output_string_line", &_Phreeqc::GetOutputStringLine)
+        .def("get_output_string_line_count", &_Phreeqc::GetOutputStringLineCount)
+        .def("get_output_string_on", &_Phreeqc::GetOutputStringOn)
+        .def("get_selected_output_column_count", &_Phreeqc::GetSelectedOutputColumnCount)
+        .def("get_selected_output_count", &_Phreeqc::GetSelectedOutputCount)
+        .def("get_selected_output_file_name", &_Phreeqc::GetSelectedOutputFileName)
+        .def("get_selected_output_file_on", &_Phreeqc::GetSelectedOutputFileOn)
+        .def("get_selected_output_row_count", &_Phreeqc::GetSelectedOutputRowCount)
+        .def("get_selected_output_string", &_Phreeqc::GetSelectedOutputString)
+        .def("get_selected_output_string_line", &_Phreeqc::GetSelectedOutputStringLine)
+        .def("get_selected_output_string_line_count", &_Phreeqc::GetSelectedOutputStringLineCount)
+        .def("get_selected_output_string_on", &_Phreeqc::GetSelectedOutputStringOn)
+        .def("get_selected_output_value", &_Phreeqc::_GetSelectedOutputValue)
+        .def("get_version_string", &_Phreeqc::GetVersionString)
+        .def("get_warning_string", &_Phreeqc::GetWarningString)
+        .def("get_warning_string_line", &_Phreeqc::GetWarningStringLine)
+        .def("get_warning_string_line_count", &_Phreeqc::GetWarningStringLineCount)
+        .def("list_components", &_Phreeqc::ListComponents)
+        .def("load_database", &_Phreeqc::LoadDatabase)
+        .def("load_database_string", &_Phreeqc::LoadDatabaseString)
+        .def("output_accumulated_lines", &_Phreeqc::OutputAccumulatedLines)
+        .def("output_error_string", &_Phreeqc::OutputErrorString)
+        .def("output_warning_string", &_Phreeqc::OutputWarningString)
+        .def("run_accumulated", &_Phreeqc::RunAccumulated)
+        .def("run_file", &_Phreeqc::RunFile)
+        .def("run_string", &_Phreeqc::RunString)
+        .def("set_current_selected_output_user_number", &_Phreeqc::SetCurrentSelectedOutputUserNumber)
+        .def("set_dump_file_name", &_Phreeqc::SetDumpFileName)
+        .def("set_dump_file_on", &_Phreeqc::SetDumpFileOn)
+        .def("set_dump_string_on", &_Phreeqc::SetDumpStringOn)
+        .def("set_error_file_name", &_Phreeqc::SetErrorFileName)
+        .def("set_error_file_on", &_Phreeqc::SetErrorFileOn)
+        .def("set_error_on", &_Phreeqc::SetErrorOn)
+        .def("set_error_string_on", &_Phreeqc::SetErrorStringOn)
+        .def("set_log_file_name", &_Phreeqc::SetLogFileName)
+        .def("set_log_file_on", &_Phreeqc::SetLogFileOn)
+        .def("set_log_string_on", &_Phreeqc::SetLogStringOn)
+        .def("set_output_file_name", &_Phreeqc::SetOutputFileName)
+        .def("set_output_file_on", &_Phreeqc::SetOutputFileOn)
+        .def("set_output_string_on", &_Phreeqc::SetOutputStringOn)
+        .def("set_selected_output_file_name", &_Phreeqc::SetSelectedOutputFileName)
+        .def("set_selected_output_file_on", &_Phreeqc::SetSelectedOutputFileOn)
+        .def("set_selected_output_string_on", &_Phreeqc::SetSelectedOutputStringOn);
+
+    m.attr("__version__") = "0.1.2";
 }
--- src/phreeqc/__init__.py
+++ src/phreeqc/__init__.py
@@ -1,5 +1,25 @@
 from __future__ import annotations
+from ._iphreeqc import _Phreeqc
 
-from ._iphreeqc import Phreeqc
+
+class Phreeqc(_Phreeqc):
+    def get_selected_output(self):
+        col_count = super().get_selected_output_column_count()
+        row_count = super().get_selected_output_row_count()
+
+        selected_output = {}
+
+        for i in range(col_count):
+            for j in range(row_count):
+                print((i, j))
+                if j == 0:
+                    col_name = super().get_selected_output_value(j, i)
+                    selected_output[col_name] = []
+                else:
+                    selected_output[col_name].append(
+                        super().get_selected_output_value(j, i)
+                    )
+        return selected_output
+
 
 __all__ = ["__doc__", "Phreeqc", "__version__"]
